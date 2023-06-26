57c4480258e2736cbdbba963460e86ce6adffc4e
JesusGarciaMtz <mazter707@gmail.com>
2023-06-25 20:38:49 -0600

    Sistema de login y registro inicial

---
+++ clases/Conexion.php
@@ -0,0 +1,20 @@
+<?php
+    class Conexion {
+
+        public $servidor = 'localhost';
+        public $usuario = 'root';
+        public $password = 'Tec.M1l3n1o.244957';
+        public $database = 'loginpoo2023';
+        public $port     = '3307';
+
+        public function conectar (){
+            return mysqli_connect(
+                $this->servidor,
+                $this->usuario,
+                $this->password,
+                $this->database,
+                $this->port
+            );
+        }
+    }
+?>
