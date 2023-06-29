fa19ff80552b0e12876feecb8e3b4ab1f28144e1
JesusGarciaMtz <mazter707@gmail.com>
2023-06-28 20:06:12 -0600

    Se agrego contenido a index.php

--- index.php
+++ index.php
@@ -2,7 +2,61 @@
 
 <?php require ('./layouts/header.php') ?>
 
-  <h1 class="mt-4">laliga</h1>
-  <p>Unidad y Flexibilidad !</p>
+  <!-- Page Content-->
+        <div class="container px-4 px-lg-5">
+            <!-- Heading Row-->
+            <div class="row gx-4 gx-lg-5 align-items-center my-5">
+                <div class="col-lg-7"><img class="img-fluid rounded mb-4 mb-lg-0" src="https://dummyimage.com/900x400/dee2e6/6c757d.jpg" alt="..." /></div>
+                <div class="col-lg-5">
+                    <h1 class="font-weight-light">laliga</h1>
+                    <p>Tu sitio de encuentro con los mejores eventos ciclistas!</p>
+                    <a class="btn btn-primary" href="eventos.php">INGRESA!</a>
+                </div>
+            </div>
+            <!-- Call to Action-->
+            <div class="card text-white bg-secondary my-5 py-4 text-center">
+                <div class="card-body"><p class="text-white m-0">PRÓXIMAS CARRERAS!</p></div>
+            </div>
+            <!-- Content Row-->
+            <div class="row gx-4 gx-lg-5">
+                <div class="col-md-4 mb-5">
+                    <div class="card h-100">
+                      <div class="card" style="width: 18rem;" >
+                        <img src="./assets/img/huracan.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                          <h5 class="card-title" style="text-align: center;">HURACÁN</h5>
+                          <p class="card-text">El tiempo se agota...</p>
+                          <a href="#" class="btn btn-primary" >Inscríbete</a>
+                        </div>
+                      </div>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-5">
+                    <div class="card h-100">
+                      <div class="card" style="width: 18rem;" >
+                        <img src="./assets/img/jonuta.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                          <h5 class="card-title" style="text-align: center;">JONUTA</h5>
+                          <p class="card-text">El reto "Guerreritos esta activo...</p>
+                          <a href="#" class="btn btn-primary" >Participa</a>
+                        </div>
+                      </div>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-5">
+                    <div class="card h-100">
+                      <div class="card" style="width: 18rem;" >
+                        <img src="./assets/img/pixan.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                          <h5 class="card-title" style="text-align: center;">PIXAN HELL</h5>
+                          <p class="card-text">Proximamente el primer maratón...</p>
+                          <a href="#" class="btn btn-primary" >Únete</a>
+                        </div>
+                      </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+        </div>
 
   <?php require ('./layouts/footer.php') ?>
