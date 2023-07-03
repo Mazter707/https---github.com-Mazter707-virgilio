a5b75324ee16184419c92db02fbda6c39d83e89f
JesusGarciaMtz <mazter707@gmail.com>
2023-07-03 12:41:09 -0600

    Se ajusto el carrusel e icono de web

--- index.php
+++ index.php
@@ -2,24 +2,18 @@
 
 <?php require ('./layouts/header.php') ?>
 
- 
+<div class="container-fluid">
             
             <!-- Background image -->
-<div class="bg-image p-5 text-center shadow-1-strong rounded mb-5 text-white"
-  style="
-    background-image: url('https://liga.nettual.com/assets/img/portada-transformed.jpeg');">
+<div class="container-fluid fondo p-5 text-center shadow-1-strong rounded mb-3 text-white">
 
-  
+    <h1 class="mb-3 h2 display-1">laliga</h1>
 
-      <h1 class="mb-3 h2 display-1">laliga</h1>
+      <p>
+      UNIDAD Y FLEXIBILIDAD
+      </p>
 
-        <p>
-        UNIDAD Y FLEXIBILIDAD
-        </p>
-
-        <a href="https://liga.nettual.com/registro.php" class="btn btn-outline-light">ÚNETE</a>
-    
-  
+    <a href="https://liga.nettual.com/registro.php" class="btn btn-outline-light">ÚNETE</a>
 
 </div>
 <!-- Background image -->
@@ -34,10 +28,10 @@
                 <div class="col-md-4 mb-5">
                     <div class="card h-100">
                       <div class="card"  >
-                        <img src="./assets/img/huracan.jpg" class="card-img-top" alt="...">
+                        <img src="./assets/img/300x480_camp.jpg" class="card-img-top" alt="...">
                         <div class="card-body">
-                        <h3 class="card-title" style="text-align: center;">HURACÁN</h3>
-                          <p class="card-text">Sábado 1 de Julio  |  Campeche</p>
+                        <h3 class="card-title" style="text-align: center;">CAMPECHE</h3>
+                          <p class="card-text " style="text-align: center;">Domingo 16 de Julio  |  Dzilbaché</p>
                           <h5 class="card-title" style="text-align: center;">$ 300.00</h5>
                           <a href="#" class="btn btn-primary mx-auto d-block" >Inscríbete</a>
                         </div>
@@ -50,8 +44,21 @@
                         <img src="./assets/img/jonuta.jpg" class="card-img-top" alt="...">
                         <div class="card-body">
                           <h3 class="card-title" style="text-align: center;">JONUTA</h3>
-                          <p class="card-text">Domingo 20 de Agosto  |  Jonuta</p>
-                          <h5 class="card-title" style="text-align: center;">$ 300.00</h5>
+                          <p class="card-text" style="text-align: center;">Domingo 20 de Agosto  |  Jonuta</p>
+                          <h5 class="card-title" style="text-align: center;">$ 155.00</h5>
+                          <a href="#" class="btn btn-primary mx-auto d-block" >Inscríbete</a>
+                        </div>
+                      </div>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-5">
+                    <div class="card h-100">
+                      <div class="card" >
+                        <img src="./assets/img/300x480_MEXICO.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                          <h3 class="card-title" style="text-align: center;">VIVA MÉXICO!</h3>
+                          <p class="card-text" style="text-align: center;">Domingo 17 de Septiembre  |  Valladolid</p>
+                          <h5 class="card-title" style="text-align: center;">$ 750.00</h5>
                           <a href="#" class="btn btn-primary mx-auto d-block" >Inscríbete</a>
                         </div>
                       </div>
@@ -62,24 +69,38 @@
                       <div class="card" >
                         <img src="./assets/img/pixan.jpg" class="card-img-top" alt="...">
                         <div class="card-body">
-                        <h3 class="card-title" style="text-align: center;">PIXAN HELL</h3>
-                          <p class="card-text">Domingo 29 de Octubre  |  Mérida</p>
+                          <h3 class="card-title" style="text-align: center;">PIXAN HELL</h3>
+                          <p class="card-text" style="text-align: center;">Domingo 29 de Octubre  |  Mérida</p>
                           <h5 class="card-title" style="text-align: center;">$ 300.00</h5>
                           <a href="#" class="btn btn-primary mx-auto d-block" >Inscríbete</a>
                         </div>
                       </div>
                     </div>
                 </div>
+                <div class="col-md-4 mb-5">
+                    <div class="card h-100">
+                      <div class="card" >
+                        <img src="./assets/img/300x480 (1).jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                          <h3 class="card-title" style="text-align: center;">EL REY DEL SUR</h3>
+                          <p class="card-text" style="text-align: center;">Domingo 19 de Noviembre  |  Tekax </p>
+                          <h5 class="card-title" style="text-align: center;">$ 750.00</h5>
+                          <a href="#" class="btn btn-primary mx-auto d-block" >Inscríbete</a>
+                        </div>
+                      </div>
+                    </div>
+                </div>
+                
                 </div>
             </div>
         </div>
         
 <!--Aqui inicia el carrusel -->
 
-<div class="container">
+<div class="container-fluid">
   <div class="card">
     <div class="card-header">
-      <h4>Outfit !</h4>
+    <h4 class="text-center display-5">Corre con estilo !</h4>
     </div>
       <div class= "card-body">
         <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
@@ -90,13 +111,15 @@
           </div>
           <div class="carousel-inner">
             <div class="carousel-item active">
+            <a href="https://www.yapur.mx/collections/2023/products/mexico-2023" target="_blank" class="btn btn-transformed stretched-link">COMPRAR</a>
               <img src="./assets/img/mexico.jpeg" class="d-block w-100" alt="Primera imagen">
-              <div class="carousel-caption d-none d-md-block">
+              <div class="carousel-caption d-none d-md-block">              
                 <h5>Celebremos fiestas patrias con este precioso Jersey !</h5>
                 <p>Fabricado con telas que te mantienen fresco y seco.</p>
               </div>
             </div>
             <div class="carousel-item">
+            <a href="https://www.yapur.mx/collections/grandes-vueltas/products/tour-2023" target="_blank" class="btn btn-transformed stretched-link">COMPRAR</a>
               <img src="./assets/img/tour-france-large.jpg" class="d-block w-100" alt="Segunda imagen">
               <div class="carousel-caption d-none d-md-block">
                 <h5>Jersey Tour de Francia</h5>
@@ -104,6 +127,7 @@
               </div>
             </div>
             <div class="carousel-item">
+            <a href="https://www.yapur.mx/collections/historicos" target="_blank" class="btn btn-transformed stretched-link">COMPRAR</a>
               <img src="./assets/img/vintage.jpeg" class="d-block w-100" alt="Tercera imagen">
               <div class="carousel-caption d-none d-md-block">
                 <h5>Sumérgete en la época dorada del ciclismo !</h5>
@@ -125,5 +149,5 @@
 </div>
 <!--Aqui termina el carrusel -->
 
-       
+
   <?php require ('./layouts/footer.php') ?>
