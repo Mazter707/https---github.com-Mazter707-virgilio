0108a53d808a7ec16076081ca6b9c4ceade5dcdb
JesusGarciaMtz <mazter707@gmail.com>
2023-07-11 22:26:56 -0600

    Se actualizo seccion de Cuenta

--- perfil.php
+++ perfil.php
@@ -3,6 +3,13 @@
 
 <div class="container-fluid">
 
+<style>
+  .content-wrapper{
+    margin-left:30px;
+    padding:30px;
+  }
+</style>
+
 <div class="d-flex" id="wrapper">
             <!-- Sidebar-->
             <div class="border-end bg-white" id="sidebar-wrapper">
@@ -83,6 +90,17 @@
                       <form id="formAccountSettings" method="POST" onsubmit="return false">
                         <div class="row">
                           <div class="mb-3 col-md-6">
+                            <label for="usuario" class="form-label">Usuario</label>
+                            <input
+                              class="form-control"
+                              type="text"
+                              id="usuario"
+                              name="usuario"
+                              value="Mazter707"
+                              autofocus
+                            />
+                          </div>
+                          <div class="mb-3 col-md-6">
                             <label for="firstName" class="form-label">Nombre completo</label>
                             <input
                               class="form-control"
@@ -90,12 +108,11 @@
                               id="firstName"
                               name="firstName"
                               value="Jesus Garcia Martinez"
-                              autofocus
                             />
                           </div>
                           <div class="mb-3 col-md-6">
                             <label for="password" class="form-label">Contraseña</label>
-                            <input class="form-control" type="password" name="password" id="lastName" value="" />
+                            <input class="form-control" type="password" name="password" id="lastName" value="********" />
                           </div>
                           <div class="mb-3 col-md-6">
                             <label for="email" class="form-label">E-mail</label>
@@ -126,64 +143,107 @@
                             <label class="form-label" for="country">Género</label>
                             <select id="country" class="select2 form-select">
                               <option value="">Selecciona</option>
-                              <option value="Australia">Australia</option>
-                              <option value="Bangladesh">Bangladesh</option>
-                              <option value="Belarus">Belarus</option>
-                              <option value="Brazil">Brazil</option>
-                              <option value="Canada">Canada</option>
-                              <option value="China">China</option>
-                              <option value="France">France</option>
-                              <option value="Germany">Germany</option>
-                              <option value="India">India</option>
-                              <option value="Indonesia">Indonesia</option>
-                              <option value="Israel">Israel</option>
-                              <option value="Italy">Italy</option>
-                              <option value="Japan">Japan</option>
-                              <option value="Korea">Korea, Republic of</option>
-                              <option value="Mexico">Mexico</option>
-                              <option value="Philippines">Philippines</option>
-                              <option value="Russia">Russian Federation</option>
-                              <option value="South Africa">South Africa</option>
-                              <option value="Thailand">Thailand</option>
-                              <option value="Turkey">Turkey</option>
-                              <option value="Ukraine">Ukraine</option>
-                              <option value="United Arab Emirates">United Arab Emirates</option>
-                              <option value="United Kingdom">United Kingdom</option>
-                              <option value="United States">United States</option>
+                              <option value="Masculino">Masculino</option>
+                              <option value="Femenino">Femenino</option>
                             </select>
                           </div>
                           <div class="mb-3 col-md-6">
                             <label for="language" class="form-label">Año de nacimiento</label>
                             <select id="language" class="select2 form-select">
                               <option value="">Selecciona</option>
-                              <option value="en">English</option>
-                              <option value="fr">French</option>
-                              <option value="de">German</option>
-                              <option value="pt">Portuguese</option>
+                              <option value="1973">1973</option>
+                              <option value="1974">1974</option>
+                              <option value="1975">1975</option>
+                              <option value="1976">1976</option>
+                              <option value="1977">1977</option>
+                              <option value="1978">1978</option>
+                              <option value="1979">1979</option>
+                              <option value="1980">1980</option>
+                              <option value="1981">1981</option>
+                              <option value="1982">1982</option>
+                              <option value="1983">1983</option>
+                              <option value="1984">1984</option>
+                              <option value="1985">1985</option>
+                              <option value="1986">1986</option>
+                              <option value="1987">1987</option>
+                              <option value="1988">1988</option>
+                              <option value="1989">1989</option>
+                              <option value="1990">1990</option>
+                              <option value="1991">1991</option>
+                              <option value="1992">1992</option>
+                              <option value="1993">1993</option>
+                              <option value="1994">1994</option>
+                              <option value="1995">1995</option>
+                              <option value="1996">1996</option>
+                              <option value="1997">1997</option>
+                              <option value="1998">1998</option>
+                              <option value="1999">1999</option>
+                              <option value="2000">2000</option>
+                              <option value="2001">2001</option>
+                              <option value="2002">2002</option>
+                              <option value="2003">2003</option>
+                              <option value="2004">2004</option>
+                              <option value="2005">2005</option>
+                              <option value="2006">2006</option>
+                              <option value="2007">2007</option>
+                              <option value="2008">2008</option>
+                              <option value="2009">2009</option>
+                              <option value="2010">2010</option>
+                              <option value="2011">2011</option>
+                              <option value="2012">2012</option>
+                              <option value="2013">2013</option>
+                              <option value="2014">2014</option>
+                              <option value="2015">2015</option>
+                              <option value="2016">2016</option>
+                              <option value="2017">2017</option>
+                    <option value="2018">2018</option>
                             </select>
                           </div>
                           <div class="mb-3 col-md-6">
                             <label for="timeZones" class="form-label">Equipo</label>
                             <select id="timeZones" class="select2 form-select">
                               <option value="">Selecciona</option>
-                              <option value="-12">(GMT-12:00) International Date Line West</option>
-                              <option value="-11">(GMT-11:00) Midway Island, Samoa</option>
-                              <option value="-10">(GMT-10:00) Hawaii</option>
-                              <option value="-9">(GMT-09:00) Alaska</option>
-                              <option value="-8">(GMT-08:00) Pacific Time (US & Canada)</option>
-                              <option value="-8">(GMT-08:00) Tijuana, Baja California</option>
-                              <option value="-7">(GMT-07:00) Arizona</option>
-                              <option value="-7">(GMT-07:00) Chihuahua, La Paz, Mazatlan</option>
-                              <option value="-7">(GMT-07:00) Mountain Time (US & Canada)</option>
-                              <option value="-6">(GMT-06:00) Central America</option>
-                              <option value="-6">(GMT-06:00) Central Time (US & Canada)</option>
-                              <option value="-6">(GMT-06:00) Guadalajara, Mexico City, Monterrey</option>
-                              <option value="-6">(GMT-06:00) Saskatchewan</option>
-                              <option value="-5">(GMT-05:00) Bogota, Lima, Quito, Rio Branco</option>
-                              <option value="-5">(GMT-05:00) Eastern Time (US & Canada)</option>
-                              <option value="-5">(GMT-05:00) Indiana (East)</option>
-                              <option value="-4">(GMT-04:00) Atlantic Time (Canada)</option>
-                              <option value="-4">(GMT-04:00) Caracas, La Paz</option>
+                              <option value="Titanes">Titanes</option>
+                              <option value="Fox Extreme">Fox Extreme</option>
+                              <option value="Horizon MTB">Horizon MTB</option>
+                              <option value="Primos Uribe y Amigos">Primos Uribe y Amigos</option>
+                              <option value="Primos Uribe">Primos Uribe</option>
+                              <option value="Campeche">Campeche</option>
+                              <option value="Brothers tem MTB">Brothers tem MTB</option>
+                              <option value="Xaos">Xaos</option>
+                              <option value="ELEGIDOSMTB">ELEGIDOSMTB</option>
+                              <option value="Seyba Playa MTB">Seyba Playa MTB</option>
+                              <option value="ARIEL TEAM _ RAPTORS">ARIEL TEAM _ RAPTORS</option>
+                              <option value="BROTHERS TEAM">BROTHERS TEAM</option>
+                              <option value="TEAM RACE HORMIGON">TEAM RACE HORMIGON</option>
+                              <option value="Miserables">Miserables</option>
+                              <option value="Fridas en Bici Campeche">Fridas en Bici Campeche</option>
+                              <option value="FRIDASCAMPECHE">FRIDASCAMPECHE</option>
+                              <option value="ARIEL TEAM_ RAPTORS">ARIEL TEAM_ RAPTORS</option>
+                              <option value="Coyotes MTB Yucatan">Coyotes MTB Yucatan</option>
+                              <option value="Familia Jaguar">Familia Jaguar</option>
+                              <option value="BROTHERS TEAM MTB">BROTHERS TEAM MTB</option>
+                              <option value="RAPTORS TEAM ARIEL">RAPTORS TEAM ARIEL</option>
+                              <option value="BIKE CLUB LEONES">BIKE CLUB LEONES</option>
+                              <option value="YAPUR.MX">YAPUR.MX</option>
+                              <option value="Mtb Campeche">Mtb Campeche</option>
+                              <option value="EL PEDAL">EL PEDAL</option>
+                              <option value="Diablos MTB">Diablos MTB</option>
+                              <option value="Triclub Perezosos">Triclub Perezosos</option>
+                              <option value="Brother team campeche mtb">Brother team campeche mtb</option>
+                              <option value="PNDJXS">PNDJXS</option>
+                              <option value="Yucatan">Yucatan</option>
+                              <option value="Cycling Adventure">Cycling Adventure</option>
+                              <option value="La Tripa MTB">La Tripa MTB</option>
+                              <option value="Chacamax racing Team">Chacamax racing Team</option>
+                              <option value="Piratas-cadencia bike team">Piratas-cadencia bike team</option>
+                              <option value="Raptors mtb Campeche">Raptors mtb Campeche</option>
+                              <option value="Becal Bikers">Becal Bikers</option>
+                              <option value="team campeche">team campeche</option>
+                              <option value="ADAMSBIKE MTB TEAM">ADAMSBIKE MTB TEAM</option>
+                              <option value="Independiente">Independiente</option>
+                              <option value="TEAM RACE HOPRMIGON">TEAM RACE HOPRMIGON</option>
+                              <option value="Bycicle mechanic/MTB Hecelchakan">Bycicle mechanic/ MTB Hecelchakan</option>
                             </select>
                           </div>
                         </div>
