45a0063f1af0df2c77c1c29e4222583409323144
JesusGarciaMtz <mazter707@gmail.com>
2023-07-12 09:55:14 -0600

    cambio de baner index

--- index.php
+++ index.php
@@ -2,21 +2,74 @@
 
 <?php require ('./layouts/header.php') ?>
 
+<style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style> 
+
 <div class="container-fluid">
             
-            <!-- Background image -->
-<div class="container-fluid fondo p-5 text-center shadow-1-strong rounded mb-3 text-white">
+<div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-indicators">
+      <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+      <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+      <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    </div>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"/></svg>
+        <img src="./assets/img/portada-transformed.jpeg" class="d-block w-100" alt="Primera imagen">
+        <div class="container">
+          <div class="carousel-caption text-start">
+            <h1>laliga</h1>
+            <p>UNIDAD Y FLEXIBILIDAD</p>
+            <p><a class="btn btn-lg btn-primary" href="#">REGISTRATE</a></p>
+          </div>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"/></svg>
 
-    <h1 class="mb-3 h2 display-1">laliga</h1>
+        <div class="container">
+          <div class="carousel-caption">
+            <h1>Another example headline.</h1>
+            <p>Some representative placeholder content for the second slide of the carousel.</p>
+            <p><a class="btn btn-lg btn-primary" href="#">Learn more</a></p>
+          </div>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"/></svg>
 
-      <p>
-      UNIDAD Y FLEXIBILIDAD
-      </p>
-
-    <a href="https://liga.nettual.com/registro.php" class="btn btn-outline-light">ÚNETE</a>
-
-</div>
-<!-- Background image -->
+        <div class="container">
+          <div class="carousel-caption text-end">
+            <h1>One more for good measure.</h1>
+            <p>Some representative placeholder content for the third slide of this carousel.</p>
+            <p><a class="btn btn-lg btn-primary" href="#">Browse gallery</a></p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
             
           
             <!-- Call to Action-->
@@ -95,9 +148,9 @@
             </div>
         </div>
         
-<!--Aqui inicia el carrusel -->
+    <!--Aqui inicia el carrusel -->
 
-<div class="container-fluid">
+  <div class="container-fluid">
   <div class="card">
     <div class="card-header">
     <h4 class="text-center display-5">Corre con estilo !</h4>
--- layouts/header.php
+++ layouts/header.php
@@ -9,6 +9,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="public/css/mi-carousel.css">
     <link href="custom.css" rel="stylesheet">
+    <link href="carousel.css" rel="stylesheet">
     <link rel="icon" href="https://liga.nettual.com/assets/img/logo-laliga.ico">
     <title>laliga | Unidad y Flexibilidad</title>
   </head>
