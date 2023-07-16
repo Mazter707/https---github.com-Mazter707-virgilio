66e715492d64c2baac2f19e4402309da2e946a79
JesusGarciaMtz <mazter707@gmail.com>
2023-07-15 18:41:13 -0600

    perfil container md anterior

--- perfil.php
+++ perfil.php
@@ -12,151 +12,151 @@
 
 <div class="d-flex mt-2 ml-0 pl-0" id="wrapper">
            
-  <!-- Sidebar -->
-  <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion mt-1 ml-0 pl-0" id="accordionSidebar">
+        <!-- Sidebar -->
+          <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion mt-1 ml-0 pl-0" id="accordionSidebar">
 
-        <!-- Divider -->
-        <hr class="sidebar-divider my-0">
+                <!-- Divider -->
+                <hr class="sidebar-divider my-0">
 
-        <!-- Nav Item - Dashboard -->
-        <li class="nav-item active">
-            <a class="nav-link" href="index.html">
-                <i class=""></i>
-                <span>Escritorio</span></a>
-        </li>
+                <!-- Nav Item - Dashboard -->
+                <li class="nav-item active">
+                    <a class="nav-link" href="index.html">
+                        <i class=""></i>
+                        <span>Escritorio</span></a>
+                </li>
 
-        <!-- Divider -->
-        <hr class="sidebar-divider">
+                <!-- Divider -->
+                <hr class="sidebar-divider">
 
-        <!-- Heading -->
-        <div class="sidebar-heading">
-            Perfil
-        </div>
-
-        <!-- Nav Item - Pages Collapse Menu -->
-        <li class="nav-item">
-            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
-                aria-expanded="true" aria-controls="collapseTwo">
-                <i class="fas fa-fw "></i>
-                <span>Mi cuenta</span>
-            </a>
-            <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-                <div class="bg-white py-2 collapse-inner rounded">
-                    <h6 class="collapse-header">Configuracion:</h6>
-                    <a class="collapse-item" href="https://liga.nettual.com/perfil.php">Detalles</a>
-                    <a class="collapse-item" href="https://liga.nettual.com/perfil.php">Ajustes</a>
+                <!-- Heading -->
+                <div class="sidebar-heading">
+                    Perfil
                 </div>
-            </div>
-        </li>
 
-        <!-- Nav Item - Utilities Collapse Menu -->
-        <li class="nav-item">
-            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
-                aria-expanded="true" aria-controls="collapseUtilities">
-                <i class="fas fa-fw "></i>
-                <span>Chat</span>
-            </a>
-            <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
-                data-parent="#accordionSidebar">
-                <div class="bg-white py-2 collapse-inner rounded">
-                    <h6 class="collapse-header">Mensajes:</h6>
-                    <a class="collapse-item" href="utilities-color.html">Directos</a>
-                    <a class="collapse-item" href="utilities-border.html">Globales</a>
-                    <a class="collapse-item" href="utilities-animation.html">Redactar</a>
-                    <a class="collapse-item" href="utilities-other.html">Promotor</a>
+                <!-- Nav Item - Pages Collapse Menu -->
+                <li class="nav-item">
+                    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
+                        aria-expanded="true" aria-controls="collapseTwo">
+                        <i class="fas fa-fw "></i>
+                        <span>Mi cuenta</span>
+                    </a>
+                    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+                        <div class="bg-white py-2 collapse-inner rounded">
+                            <h6 class="collapse-header">Configuracion:</h6>
+                            <a class="collapse-item" href="https://liga.nettual.com/perfil.php">Detalles</a>
+                            <a class="collapse-item" href="https://liga.nettual.com/perfil.php">Ajustes</a>
+                        </div>
+                    </div>
+                </li>
+
+                <!-- Nav Item - Utilities Collapse Menu -->
+                <li class="nav-item">
+                    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
+                        aria-expanded="true" aria-controls="collapseUtilities">
+                        <i class="fas fa-fw "></i>
+                        <span>Chat</span>
+                    </a>
+                    <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
+                        data-parent="#accordionSidebar">
+                        <div class="bg-white py-2 collapse-inner rounded">
+                            <h6 class="collapse-header">Mensajes:</h6>
+                            <a class="collapse-item" href="utilities-color.html">Directos</a>
+                            <a class="collapse-item" href="utilities-border.html">Globales</a>
+                            <a class="collapse-item" href="utilities-animation.html">Redactar</a>
+                            <a class="collapse-item" href="utilities-other.html">Promotor</a>
+                        </div>
+                    </div>
+                </li>
+
+                <!-- Divider -->
+                <hr class="sidebar-divider">
+
+                <!-- Heading -->
+                <div class="sidebar-heading">
+                    Eventos
                 </div>
-            </div>
-        </li>
 
-        <!-- Divider -->
-        <hr class="sidebar-divider">
+                
 
-        <!-- Heading -->
-        <div class="sidebar-heading">
-            Eventos
-        </div>
+                <!-- Nav Item - Charts -->
+                <li class="nav-item">
+                    <a class="nav-link" href="charts.html">
+                        <i class="fas fa-fw "></i>
+                        <span>Próximos</span></a>
+                </li>
 
-        
+                <!-- Nav Item - Tables -->
+                <li class="nav-item">
+                    <a class="nav-link" href="tables.html">
+                        <i class="fas fa-fw "></i>
+                        <span>Pasados</span></a>
+                </li>
 
-        <!-- Nav Item - Charts -->
-        <li class="nav-item">
-            <a class="nav-link" href="charts.html">
-                <i class="fas fa-fw "></i>
-                <span>Próximos</span></a>
-        </li>
+                <!-- Divider -->
+                <hr class="sidebar-divider d-none d-md-block">
 
-        <!-- Nav Item - Tables -->
-        <li class="nav-item">
-            <a class="nav-link" href="tables.html">
-                <i class="fas fa-fw "></i>
-                <span>Pasados</span></a>
-        </li>
+                <!-- Heading -->
+                <div class="sidebar-heading">
+                    Resultados
+                </div>
 
-        <!-- Divider -->
-        <hr class="sidebar-divider d-none d-md-block">
+                
 
-        <!-- Heading -->
-        <div class="sidebar-heading">
-            Resultados
-        </div>
+                <!-- Nav Item - Charts -->
+                <li class="nav-item">
+                    <a class="nav-link" href="charts.html">
+                        <i class="fas fa-fw "></i>
+                        <span>Personales</span></a>
+                </li>
 
-        
+                <!-- Nav Item - Tables -->
+                <li class="nav-item">
+                    <a class="nav-link" href="tables.html">
+                        <i class="fas fa-fw "></i>
+                        <span>Equipo</span></a>
+                </li>
 
-        <!-- Nav Item - Charts -->
-        <li class="nav-item">
-            <a class="nav-link" href="charts.html">
-                <i class="fas fa-fw "></i>
-                <span>Personales</span></a>
-        </li>
+                <!-- Divider -->
+                <hr class="sidebar-divider d-none d-md-block">
+                <!-- Heading -->
+                <div class="sidebar-heading">
+                    Mercado
+                </div>
 
-        <!-- Nav Item - Tables -->
-        <li class="nav-item">
-            <a class="nav-link" href="tables.html">
-                <i class="fas fa-fw "></i>
-                <span>Equipo</span></a>
-        </li>
+                
 
-        <!-- Divider -->
-        <hr class="sidebar-divider d-none d-md-block">
-        <!-- Heading -->
-        <div class="sidebar-heading">
-            Mercado
-        </div>
+                <!-- Nav Item - Charts -->
+                <li class="nav-item">
+                    <a class="nav-link" href="charts.html">
+                        <i class="fas fa-fw"></i>
+                        <span>Mis compras</span></a>
+                </li>
 
-        
+                <!-- Nav Item - Tables -->
+                <li class="nav-item">
+                    <a class="nav-link" href="tables.html">
+                        <i class="fas fa-fw"></i>
+                        <span>Mis ventas</span></a>
+                </li>
 
-        <!-- Nav Item - Charts -->
-        <li class="nav-item">
-            <a class="nav-link" href="charts.html">
-                <i class="fas fa-fw"></i>
-                <span>Mis compras</span></a>
-        </li>
-
-        <!-- Nav Item - Tables -->
-        <li class="nav-item">
-            <a class="nav-link" href="tables.html">
-                <i class="fas fa-fw"></i>
-                <span>Mis ventas</span></a>
-        </li>
-
-        <!-- Divider -->
-        <hr class="sidebar-divider d-none d-md-block">
-  </ul>
-<!-- End of Sidebar -->
+                <!-- Divider -->
+                <hr class="sidebar-divider d-none d-md-block">
+          </ul>
+        <!-- End of Sidebar -->
             <!-- Content wrapper -->
           <div class="content-wrapper">
             <!-- Content -->
 
             <div class="container-xxl flex-grow-1 container-p-y">
               <div class="row">
-                <div class="col-sm-12">
+                <div class="col-md-12">
                   <div class="card mb-4">
                        <!-- Page Heading -->
                     <div class="d-sm-flex align-items-center  mb-4">
                         <!-- Sidebar Toggle (Topbar) -->
-                    <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-1">
-                        <i class="fa fa-bars"></i>
-                    </button>
+                        <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-1">
+                            <i class="fa fa-bars"></i>
+                        </button>
                         <h1 class="h3 mb-0 text-gray-800 text-center">Perfil</h1>
                     </div>
                     <!-- Account -->
@@ -172,8 +172,8 @@
                         />
                         <div class="button-wrapper">
                           <label for="upload" class="btn btn-primary me-2 mb-4" tabindex="0">
-                            <span class="d-none d-sm-block">Actualizar foto</span>
-                            <i class="bx bx-upload d-block d-sm-none"></i>
+                            <span class="d-block">Actualizar foto</span>
+                            <i class="bx bx-upload d-block "></i>
                             <input
                               type="file"
                               id="upload"
@@ -191,7 +191,7 @@
                     <div class="card-body">
                       <form id="formAccountSettings" method="POST" onsubmit="return false">
                         <div class="row">
-                          <div class="mb-3 col-sm-6">
+                          <div class="mb-3 col-md-6">
                             <label for="usuario" class="form-label">Usuario</label>
                             <input
                               class="form-control"
@@ -202,7 +202,7 @@
                               autofocus
                             />
                           </div>
-                          <div class="mb-3 col-sm-6">
+                          <div class="mb-3 col-md-6">
                             <label for="firstName" class="form-label">Nombre completo</label>
                             <input
                               class="form-control"
@@ -212,11 +212,11 @@
                               value="Jesus Garcia Martinez"
                             />
                           </div>
-                          <div class="mb-3 col-sm-6">
+                          <div class="mb-3 col-md-6">
                             <label for="password" class="form-label">Contraseña</label>
                             <input class="form-control" type="password" name="password" id="lastName" value="********" />
                           </div>
-                          <div class="mb-3 col-sm-6">
+                          <div class="mb-3 col-md-6">
                             <label for="email" class="form-label">E-mail</label>
                             <input
                               class="form-control"
@@ -227,7 +227,7 @@
                               placeholder="jmazter707@nettual.com"
                             />
                           </div>
-                          <div class="mb-3 col-sm-6">
+                          <div class="mb-3 col-md-6">
                             <label class="form-label" for="phoneNumber">Celular</label>
                             <div class="input-group input-group-merge">
                               <span class="input-group-text">MX (+52)</span>
@@ -241,7 +241,7 @@
                             </div>
                           </div>
                           
-                          <div class="mb-3 col-sm-6">
+                          <div class="mb-3 col-md-6">
                             <label class="form-label" for="country">Género</label>
                             <select id="country" class="select2 form-select">
                               <option value="">Selecciona</option>
@@ -249,7 +249,7 @@
                               <option value="Femenino">Femenino</option>
                             </select>
                           </div>
-                          <div class="mb-3 col-sm-6">
+                          <div class="mb-3 col-md-6">
                             <label for="language" class="form-label">Año de nacimiento</label>
                             <select id="language" class="select2 form-select">
                               <option value="">Selecciona</option>
@@ -301,7 +301,7 @@
                     <option value="2018">2018</option>
                             </select>
                           </div>
-                          <div class="mb-3 col-sm-6">
+                          <div class="mb-3 col-md-6">
                             <label for="timeZones" class="form-label">Equipo</label>
                             <select id="timeZones" class="select2 form-select">
                               <option value="">Selecciona</option>
@@ -385,6 +385,6 @@
                 </div>
               </div>
             </div>
-        </div>
+          </div>
 </div>
   <?php require ('./layouts/footer.php') ?>
